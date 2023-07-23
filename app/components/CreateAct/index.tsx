0e09d66e5b5ce1034a33a79915d74013d61f24ae
Chanh Nguyen <hello@chanh.design>
2023-07-23 14:57:38 -0600

    Initial CreateAlert styyyyles

--- app/components/CreateAct/index.tsx
+++ app/components/CreateAct/index.tsx
@@ -1,5 +1,7 @@
 import { useForm } from 'react-hook-form';
 import { useCreateActMutation } from '@/app/lib/hooks';
+import styles from './styles.module.scss';
+import buttonStyles from '@/styles/button.module.scss';
 
 interface FormData {
   name: string;
@@ -15,17 +17,21 @@ export default function CreateAct() {
   const { mutate: createAct } = useCreateActMutation();
 
   return (
-    <form
-      onSubmit={handleSubmit((values) =>
-        createAct(values, { onSuccess: () => reset() })
-      )}
-    >
-      <label htmlFor="name">Act name</label>
-      <input type="text" {...register('name', { required: true })} />
-      {errors.name?.type === 'required' && (
-        <p role="alert">Act name is required</p>
-      )}
-      <button>Create act</button>
-    </form>
+    <section className={styles.container}>
+      <h2 className={styles.heading}>Create a new act</h2>
+      <form
+        className={styles.form}
+        onSubmit={handleSubmit((values) =>
+          createAct(values, { onSuccess: () => reset() })
+        )}
+      >
+        <label htmlFor="name">Act name</label>
+        <input type="text" {...register('name', { required: true })} />
+        {errors.name?.type === 'required' && (
+          <p role="alert">Act name is required</p>
+        )}
+        <button className={buttonStyles.btn}>Create act</button>
+      </form>
+    </section>
   );
 }
